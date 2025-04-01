8981d5e14e3ebabdf3a0c2f16b4f050197a306e3
Mritunjay Rai <146355352+railav61@users.noreply.github.com>
2025-04-01 21:41:52 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonial from "@/components/MusicSchoolTestimonial";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
-import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
-      <WhyChooseUs />
       <MusicSchoolTestimonial />
       <UpcomingWebinars />
       <Instructors />
